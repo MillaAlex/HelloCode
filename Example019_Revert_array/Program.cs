115a578f18c11b1ad301db3ecd660831d54479d1
Lyudmila <liudmila.k1610@gmail.com>
2023-01-31 20:29:58 +0300

    reverse array - new cycle option

---
+++ Example019_Revert_array/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+Console.WriteLine("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение массива: ");
+int minArr = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение массива: ");
+int maxArr = Convert.ToInt32(Console.ReadLine());
+
+int[] GetArr(int n)
+{
+    int[] arr = new int[n];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minArr, maxArr + 1);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void ReverseArray(int[] arr)
+{
+    for (int i = arr.Length - 1; i >= 0; i--)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] array = GetArr(n);
+PrintArr(array);
+ReverseArray(array);
