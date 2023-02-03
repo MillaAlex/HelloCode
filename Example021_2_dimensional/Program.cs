2224f1fb1686a9ac2b4134b6267e1bcd48d3c14b
Lyudmila <liudmila.k1610@gmail.com>
2023-02-03 23:33:37 +0300

    2-dim array, changes in array

---
+++ Example021_2_dimensional/Program.cs
@@ -0,0 +1,221 @@
+﻿// // Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// // m = 3, n = 4.
+// // 1 4 8 19
+// // 5 -2 33 -2
+// // 77 3 8 1
+
+// Console.Write("Введите размер m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите размер n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] arr = new int[m, n];
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arr.GetLength(1); j++)
+//     {
+//         arr[i, j] = new Random().Next(-9, 10);
+//     }
+// }
+// PrintArr(arr);
+// void PrintArr(int[,] arry)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             // Console.Write($"{arry[i, j]}\t");   // \t = табуляция
+//             Console.Write($"{arry[i, j], 4}"); // аккуратный вывод в таблицу
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+
+// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится
+// по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+// Console.Write("Введите размер m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите размер n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int[,] arr = new int[m, n];
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arr.GetLength(1); j++)
+//     {
+//         arr[i, j] = i + j;
+//     }
+// }
+// PrintArr(arr);
+// void PrintArr(int[,] arry)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             // Console.Write($"{arry[i, j]}\t");   // \t = табуляция
+//             Console.Write($"{arry[i, j], 4}"); // аккуратный вывод в таблицу
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса не чётные,
+// и замените эти элементы на их квадраты.Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+int[,] arr = new int[3, 4];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = new Random().Next(1, 10);
+    }
+}
+PrintArr(arr);
+void PrintArr(int[,] arry)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arry[i, j], 4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void NewArray(int[,] arry)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i % 2 == 1 && j % 2 == 1)
+            {
+                arr[i, j] = arr[i, j] * arr[i, j];
+            }
+            Console.Write($"{arry[i, j], 4}");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine();
+NewArray(arr);
+
+
+
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся
+// на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] arr = new int[3, 4];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = new Random().Next(1, 10);
+    }
+}
+PrintArr(arr);
+void PrintArr(int[,] arry)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arry[i, j], 4}");
+        }
+        Console.WriteLine();
+    }
+}
+int Sum(int[,] arry)
+{
+    int result = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                result = result + arr[i, j];
+            }
+        }
+    }
+    return result;
+}
+Console.WriteLine();
+Console.Write($"Сумма элементов главной диагонали: {Sum(arr)}");
+
+
+
+
+int[,] array = new int[5, 5];
+array = FillArray(array);
+
+
+int[,] FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return arr;
+}
+
+
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(array);
+
+void DiogonalSumm(int [,] arr)
+{
+    int sum = 0;
+    Console.WriteLine("Summ of diogonal number: ");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+           if(i==j)
+           {
+            Console.Write($"{arr[i,j]} + ");
+            sum +=  arr[i,j];
+           }
+        }
+     }
+     Console.Write("\b\b= ");
+     Console.Write($"{sum}");
+}
