40ca4fecfca9d0f72b72307f8a59c83f7f0015dd
Lyudmila <liudmila.k1610@gmail.com>
2023-02-05 14:23:05 +0300

    Number and its figures, multiplication 1 to N, random 0 & 1

--- Selfwork26/Program.cs
+++ Selfwork26/Program.cs
@@ -24,19 +24,19 @@ Console.WriteLine($"Количество цифр в числе {number} -> {(Qu
 // 4 -> 24 
 // 5 -> 120
 
-// Console.Write("Введите число: ");
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-// int Multiplication (int N)
-// {
-//     int count = 1;
-//     for (int i = 1; i <= N; i++)
-//     {
-//         count = count * i;        
-//     }
-//     return count;
-// }
-// Console.WriteLine(Multiplication(n));
+int Multiplication (int N)
+{
+    int count = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        count = count * i;        
+    }
+    return count;
+}
+Console.WriteLine(Multiplication(n));
 
 
 
@@ -44,54 +44,48 @@ Console.WriteLine($"Количество цифр в числе {number} -> {(Qu
 // единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-// int[] random_array = new int[8];
-// for (int i = 0; i < random_array.Length; i++)
-// {
-//     random_array[i] = new Random().Next(0,2);
-//     Console.Write(random_array[i]);
-// }
+int[] random_array = new int[8];
+for (int i = 0; i < random_array.Length; i++)
+{
+    random_array[i] = new Random().Next(0,2);
+    Console.Write(random_array[i]);
+}
+int[] array = new int[8];
+FillArray(array);
+ShowArray(array);
+void FillArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+}
+void ShowArray(int[] arr)
+{
+    foreach (int element in arr)
+    {
+        Console.Write($"{element} ");
+    }
+}
 
 
 
-// int[] array = new int[8];
-// FillArray(array);
-// ShowArray(array);
-
-// void FillArray(int[] arr)
-// {
-//     for(int i = 0; i < arr.Length; i++)
-//     {
-//         arr[i] = new Random().Next(0, 2);
-//     }
-// }
-// void ShowArray(int[] arr)
-// {
-//     foreach (int element in arr)
-//     {
-//         Console.Write($"{element} ");
-//     }
-// }
-
-
-
-// int[] BitArray()
-// {
-//     int[] array = new int[8];
-//     Random rand = new Random();
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         array[i] = rand.Next(0, 2);
-//     }
-//     return array;
-// }
-
-// void Print(int[] arr)
-// {
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         Console.WriteLine($"arr[{i}] = {arr[i]}");
-//     }
-// }
-
-// int[] a = BitArray();
-// Print(a);
+int[] BitArray()
+{
+    int[] array = new int[8];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0, 2);
+    }
+    return array;
+}
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"arr[{i}] = {arr[i]}");
+    }
+}
+int[] a = BitArray();
+Print(a);
