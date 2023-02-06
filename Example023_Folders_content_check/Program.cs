ce0b751ea8ecb5c4fedacc11c4b7bc9bc0eee59d
Lyudmila <liudmila.k1610@gmail.com>
2023-02-06 22:04:14 +0300

    recoursing - folders content check

---
+++ Example023_Folders_content_check/Program.cs
@@ -0,0 +1,15 @@
+﻿void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalogs = new DirectoryInfo(path);
+    foreach (var currentCatalog in catalogs.GetDirectories())
+    {
+        Console.WriteLine($"{indent} {currentCatalog.Name}");
+        CatalogInfo(currentCatalog.FullName, indent + " ");
+    }
+    foreach (var item in catalogs.GetFiles())
+    {
+        Console.WriteLine($"{indent} {item.Name}");
+    }
+}
+string path = @"C:\Examples\Example001_HelloConsole";
+CatalogInfo(path);
