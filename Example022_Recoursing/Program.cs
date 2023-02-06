9c457c2f0781ebd59407f1aa35d2556093b9910e
Lyudmila <liudmila.k1610@gmail.com>
2023-02-06 21:52:56 +0300

    recoursing vs for

---
+++ Example022_Recoursing/Program.cs
@@ -0,0 +1,38 @@
+﻿string NumbersRec(int a, int b)    //Recoursing
+{
+    if (a <= b) return ($"{a} " + NumbersRec(a + 1, b));
+    else return String.Empty;
+}
+Console.WriteLine(NumbersRec(1, 10));
+
+
+string NumbersFor(int a, int b)    //cycle FOR
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += ($"{i} ");
+    }
+    return result;
+}
+Console.WriteLine(NumbersFor(1, 10));
+
+
+string NumbersRec(int a, int b)    //Recoursing a>b
+{
+    if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+    else return String.Empty;
+}
+Console.WriteLine(NumbersRec(1, 10));
+
+
+string NumbersFor(int a, int b)    //cycle FOR a>b
+{
+    string result = String.Empty;
+    for (int i = a; i >= b; i--)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+Console.WriteLine(NumbersFor(10, 1));
